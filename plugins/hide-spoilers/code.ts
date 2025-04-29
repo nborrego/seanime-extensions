a2e11e659d325c40df9f8c38e1450583e61f7892
5rahim <bonfohzaki@gmail.com>
2025-04-29 20:48:36 +0000

    update

--- plugins/hide-spoilers/code.ts
+++ plugins/hide-spoilers/code.ts
@@ -192,7 +192,6 @@ function init() {
                             continue
                         }
 
-                        console.log(titleSelection.attr("id"))
                         const title = ctx.dom.asElement(titleSelection.attr("id")!)
                         if (hideTitles && episodeNumber > progress) {
                             title?.setStyle("filter", "blur(4px)")
